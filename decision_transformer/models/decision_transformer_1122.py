cfd0e4193a4e88a77a866ae25f5d78fe7972aa09
Ray Huang <rayray2002.huang@gmail.com>
2023-06-09 00:56:09 +0800

    Add masking and update reward function (#2)
    * add mask
    
    * update reward function

--- decision_transformer/models/decision_transformer_1122.py
+++ decision_transformer/models/decision_transformer_1122.py
@@ -13,35 +13,31 @@ class RewardEncoder(nn.Module):
         self.embed_dim = embed_dim
         self.num_rws = num_rws
         self.mask_value = mask_value
-        self.fcs = nn.ModuleList(
-            [torch.nn.Linear(1, embed_dim) for _ in range(num_rws)]
-        )
-        self.activation = torch.nn.ReLU()
-        self.attention = torch.nn.MultiheadAttention(
-            embed_dim, num_heads=1, batch_first=True
-        )
-        self.query = torch.nn.Parameter(torch.randn(1, 1, embed_dim))
+        self.fcs = nn.ModuleList([nn.Linear(1, embed_dim) for _ in range(num_rws)])
+        self.activation = nn.ReLU()
+        self.attention = nn.MultiheadAttention(embed_dim, num_heads=1, batch_first=True)
+        self.query = nn.Parameter(torch.randn(1, 1, embed_dim))
 
-    def forward(self, rs):
+    def forward(self, rewards):
         # if r is -100, then it is masked
-        bs, length, _ = rs.shape
-        attn_mask = rs == self.mask_value
-        if (attn_mask + (rs == 0)).all():
-            return torch.zeros(bs, length, self.embed_dim).to(rs.device)
+        batch_size, length, _ = rewards.shape
+        attn_mask = rewards == self.mask_value
+        if (attn_mask + (rewards == 0)).all():
+            return torch.zeros(batch_size, length, self.embed_dim).to(rewards.device)
 
-        rs = rs.unsqueeze(2)
-        rs = torch.stack(
-            [self.fcs[i](rs[:, :, :, i]) for i in range(self.num_rws)], dim=-2
+        rewards = rewards.unsqueeze(2)
+        rewards = torch.stack(
+            [self.fcs[i](rewards[:, :, :, i]) for i in range(self.num_rws)], dim=-2
         )
-        rs = self.activation(rs)
+        rewards = self.activation(rewards)
 
-        rs = rs.view(-1, self.num_rws, self.embed_dim)
+        rewards = rewards.view(-1, self.num_rws, self.embed_dim)
         attn_mask = attn_mask.view(-1, self.num_rws).unsqueeze(1)
-        q = self.query.expand(rs.shape[0], 1, self.embed_dim)
+        q = self.query.expand(rewards.shape[0], 1, self.embed_dim)
 
-        out, att_map = self.attention(q, rs, rs, attn_mask=attn_mask)
+        out, att_map = self.attention(q, rewards, rewards, attn_mask=attn_mask)
 
-        return out.view(bs, length, -1), att_map
+        return out.view(batch_size, length, -1), att_map
 
 
 class TrajectoryModel(nn.Module):
@@ -95,30 +91,39 @@ class DecisionTransformer(TrajectoryModel):
         self.embed_timestep = nn.Embedding(max_ep_len, hidden_size)
 
         if kwargs.get("reward_encoder", None) == "linear":
+            print("Using linear reward encoder")
             self.embed_return = nn.Linear(return_dim, hidden_size)
         else:
+            print("Using MLP reward encoder")
             self.embed_return = RewardEncoder(num_rws=return_dim, embed_dim=hidden_size)
         # self.embed_return = nn.Linear(return_dim, hidden_size)
+        self.embed_mask = nn.Linear(return_dim, hidden_size)
 
-        self.embed_state = torch.nn.Linear(self.state_dim, hidden_size)
-        self.embed_action = torch.nn.Linear(self.act_dim, hidden_size)
+        self.embed_state = nn.Linear(self.state_dim, hidden_size)
+        self.embed_action = nn.Linear(self.act_dim, hidden_size)
 
         self.embed_ln = nn.LayerNorm(hidden_size)
 
         # note: we don't predict states or returns for the paper
-        self.predict_state = torch.nn.Linear(hidden_size, self.state_dim)
+        self.predict_state = nn.Linear(hidden_size, self.state_dim)
         self.predict_action = nn.Sequential(
             *(
                 [nn.Linear(hidden_size, self.act_dim)]
                 + ([nn.Tanh()] if action_tanh else [])
             )
         )
-        self.predict_return = torch.nn.Linear(hidden_size, self.return_dim)
+        self.predict_return = nn.Linear(hidden_size, self.return_dim)
 
     def forward(
-        self, states, actions, rewards, returns_to_go, timesteps, attention_mask=None
+        self,
+        states,
+        actions,
+        rewards,
+        returns_to_go,
+        timesteps,
+        task_masks,
+        attention_mask=None,
     ):
-
         batch_size, seq_length = states.shape[0], states.shape[1]
 
         if attention_mask is None:
@@ -128,6 +133,7 @@ class DecisionTransformer(TrajectoryModel):
         # embed each modality with a different head
         state_embeddings = self.embed_state(states)
         action_embeddings = self.embed_action(actions)
+        task_embeddings = self.embed_mask(task_masks)
 
         returns_embeddings = self.embed_return(returns_to_go)[0]
 
@@ -136,24 +142,34 @@ class DecisionTransformer(TrajectoryModel):
         # time embeddings are treated similar to positional embeddings
         state_embeddings = state_embeddings + time_embeddings
         action_embeddings = action_embeddings + time_embeddings
+        task_embeddings = task_embeddings + time_embeddings
         returns_embeddings = returns_embeddings + time_embeddings
 
         # this makes the sequence look like (R_1, s_1, a_1, R_2, s_2, a_2, ...)
         # which works nice in an autoregressive sense since states predict actions
         stacked_inputs = (
             torch.stack(
-                (returns_embeddings, state_embeddings, action_embeddings), dim=1
-            )
-            .permute(0, 2, 1, 3)
-            .reshape(batch_size, 3 * seq_length, self.hidden_size)
+                (
+                    task_embeddings,
+                    returns_embeddings,
+                    state_embeddings,
+                    action_embeddings,
+                ),
+                dim=1,
+            )  # [64, 4, 20, 256]
+            .permute(0, 2, 1, 3)  # [64, 20, 4, 256]
+            .reshape(batch_size, 4 * seq_length, self.hidden_size)  # [64, 80, 256]
         )
+        # print(stacked_inputs.shape)
         stacked_inputs = self.embed_ln(stacked_inputs)
 
         # to make the attention mask fit the stacked inputs, have to stack it as well
         stacked_attention_mask = (
-            torch.stack((attention_mask, attention_mask, attention_mask), dim=1)
+            torch.stack(
+                (attention_mask, attention_mask, attention_mask, attention_mask), dim=1
+            )
             .permute(0, 2, 1)
-            .reshape(batch_size, 3 * seq_length)
+            .reshape(batch_size, 4 * seq_length)
         )
 
         # we feed in the input embeddings (not word indices as in NLP) to the model
@@ -165,7 +181,7 @@ class DecisionTransformer(TrajectoryModel):
 
         # reshape x so that the second dimension corresponds to the original
         # returns (0), states (1), or actions (2); i.e. x[:,1,t] is the token for s_t
-        x = x.reshape(batch_size, seq_length, 3, self.hidden_size).permute(0, 2, 1, 3)
+        x = x.reshape(batch_size, seq_length, 4, self.hidden_size).permute(0, 2, 1, 3)
 
         # get predictions
         return_preds = self.predict_return(
@@ -178,19 +194,23 @@ class DecisionTransformer(TrajectoryModel):
 
         return state_preds, action_preds, return_preds
 
-    def get_action(self, states, actions, rewards, returns_to_go, timesteps, **kwargs):
+    def get_action(
+        self, states, actions, rewards, returns_to_go, timesteps, task_masks, **kwargs
+    ):
         # we don't care about the past rewards in this model
 
         states = states.reshape(1, -1, self.state_dim)
         actions = actions.reshape(1, -1, self.act_dim)
         returns_to_go = returns_to_go.reshape(1, -1, self.return_dim)
         timesteps = timesteps.reshape(1, -1)
+        task_masks = task_masks.reshape(1, -1, self.return_dim)
 
         if self.max_length is not None:
             states = states[:, -self.max_length :]
             actions = actions[:, -self.max_length :]
             returns_to_go = returns_to_go[:, -self.max_length :]
             timesteps = timesteps[:, -self.max_length :]
+            task_masks = task_masks[:, -self.max_length :]
 
             # pad all tokens to sequence length
             attention_mask = torch.cat(
@@ -254,6 +274,20 @@ class DecisionTransformer(TrajectoryModel):
                 ],
                 dim=1,
             ).to(dtype=torch.long)
+            task_masks = torch.cat(
+                [
+                    torch.zeros(
+                        (
+                            task_masks.shape[0],
+                            self.max_length - task_masks.shape[1],
+                            self.return_dim,
+                        ),
+                        device=task_masks.device,
+                    ),
+                    task_masks,
+                ],
+                dim=1,
+            ).to(dtype=torch.float32)
         else:
             attention_mask = None
 
@@ -263,6 +297,7 @@ class DecisionTransformer(TrajectoryModel):
             None,
             returns_to_go,
             timesteps,
+            task_masks,
             attention_mask=attention_mask,
             **kwargs
         )
--- mtdt/test.py
+++ mtdt/test.py
@@ -5,31 +5,37 @@ import os
 
 np.set_printoptions(precision=3)
 
-model_path = "handcraft_ckpt/checkpoint_52.pt"
+# ========================================
+ckpt_path = "three_full_ckpt"
+checkpoint = "checkpoint_6.pt"
+num_epochs = 10
+task_mask = np.array([1, 1, 0])
+target_return = np.array([11.5, 10, 0])
+# ========================================
 
+model_path = os.path.join(ckpt_path, checkpoint)
 model.load_state_dict(torch.load(model_path))
 model.eval()
 print("Model loaded successfully")
 
-env_class = getattr(environments, config["general"]["env_name"])
-env = env_class(render_mode="rgb_array")
+# env_class = getattr(environments, config["general"]["env_name"])
+# env = env_class(render_mode="rgb_array")
 
-if not os.path.exists("gifs"):
-    os.mkdir("gifs")
-
-target_return = np.array([0.95])
+if not os.path.exists(f"{ckpt_path}/gifs"):
+    os.system(f"mkdir -p {ckpt_path}/gifs")
 
 actual_return = []
 return_error = []
 frames = []
-for k in tqdm(range(100)):
+for k in tqdm(range(num_epochs)):
     seed = random.randint(0, 100000)
     env.reset(seed=seed)
-    render = len(frames) < 10
+    render = len(frames) < 5
 
     result = eval(
         model=model,
         env=env,
+        task_mask=task_mask,
         rw=target_return,
         seed=seed,
         temperature=1.0,
@@ -38,19 +44,24 @@ for k in tqdm(range(100)):
     actual_return.append(result["actual_return"])
     return_error.append(abs(target_return - actual_return[-1]))
 
-    if render:
+    if render and actual_return[-1][0] < 10:
         frame = result["frames"]
         frame = np.concatenate([frame] + [frame[-1:] for _ in range(5)], axis=0)
         frames.append(frame)
+
+actual_return.sort(key=lambda x: (x[0], x[1], x[2]), reverse=True)
+for i in range(len(actual_return)):
+    print(actual_return[i])
 
 actual_return = np.mean(actual_return, axis=0)
 return_error_mean = np.mean(return_error, axis=0)
 return_error_std = np.std(return_error, axis=0)
 frames = np.concatenate(frames, axis=0)
-print(frames.shape)
+# print(frames.shape)
 
-print(f"Seed: {seed}")
-print(f"Episode length: {result['episode_length']}")
+# print(f"Seed: {seed}")
+# print(f"Episode length: {result['episode_length']}")
+print(f"Task mask:\t{task_mask}")
 print(f"Target return:\t{target_return}")
 print(f"Actual return:\t{actual_return}")
 print(f"Return error mean:\t{return_error_mean}")
@@ -59,7 +70,9 @@ print(f"Action sequence: {result['actions']}")
 
 # save the result as gif
 print("Rendering GIF")
-display_frames_as_gif(frames, name=f"gifs/{target_return}_{actual_return}.gif")
+display_frames_as_gif(
+    frames, name=f"{ckpt_path}/gifs/{target_return}_{actual_return}.gif"
+)
 
 # render_gif(
 #     env,
